418d566417fceeaa9d53714372545b51d40a5c82
wlgg <wgang316@163.com>
2015-10-20 16:39:56 +0800

    Initial Commit

---
+++ WGText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGText
+//
+//  Created by 王刚 on 15/10/20.
+//  Copyright © 2015年 王刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WGText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WGText
+//
+//  Created by 王刚 on 15/10/20.
+//  Copyright © 2015年 王刚. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
